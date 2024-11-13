e4dccc721a5887c5b7e80d0e9e705f171ef6f98d
shindev285 <vhcity285@gmail.com>
2024-11-13 09:57:51 +0700

    login

--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -12,7 +12,7 @@ const register = async (req: any, res: any) => {
     // email phải là duu nhất
     const user = await UserModel.findOne({ email });
     if (user) {
-      throw new Error(`Email ${email} không tồn tại`);
+      throw new Error(`Email ${email} đã tồn tại`);
     }
     // hassparword
     const salt = await bcrypt.genSalt(10);
@@ -21,7 +21,6 @@ const register = async (req: any, res: any) => {
     const newUser: any = new UserModel(body);
     await newUser.save();
     // //  Xóa password
-    // delete  newUser.password;
     delete newUser._doc.password;
 
     // Trả về phản hồi thành công với mã trạng thái 200
@@ -39,11 +38,43 @@ const register = async (req: any, res: any) => {
   } catch (error: any) {
     // Trả về mã lỗi 404 và phản hồi lỗi
     res.status(404).json({
-      message: "Request failed with status code 404",
-      status: 404,
-      error: error.message || "Not Found",
+			message: error.message,
+		});
+  }
+};
+const login = async (req: any, res: any) => {
+  const body = req.body;
+  const {  email, password } = body;
+
+  try {
+    const user: any = await UserModel.findOne({ email });
+    if (!user) {
+      throw new Error(`Email ${email} không tồn tại`);
+    }
+    const isMatchPassword = await bcrypt.compare(body.password, user.password);
+    if (!isMatchPassword) {
+      throw new Error(`Đăng Nhập Thất Bại!!! vui lòng kiểm tra lại email/password`);
+    }
+    delete user._doc.password;
+
+    // Trả về phản hồi thành công với mã trạng thái 200
+    res.status(200).json({
+      message: "Đăng Nhập Thành Công !!!",
+      data: {
+        ...user._doc,
+        token: await getAccessToken({
+          _id: user._id,
+          email: user.email,
+          rule: user.rule ?? 1,
+        }),
+      },
     });
+  } catch (error: any) {
+    // Trả về mã lỗi 404 và phản hồi lỗi
+    res.status(404).json({
+			message: error.message,
+		});
   }
 };
 
-export { register };
+export { register,login };
--- src/models/UserModel.ts
+++ src/models/UserModel.ts
@@ -14,6 +14,10 @@ const userShema = new Schema({
     type: String,
     required: true,
   },
+  rule:{
+    type: Number,
+    default: 1
+  },
   createdAt: {
     type: Date,
     default: Date.now(),
